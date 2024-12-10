a8fcdfd265f11f1e5bc64ea2c7b730f4745760ea
Andika Pras <74967731+andika13194@users.noreply.github.com>
2024-12-10 14:46:58 +0700

    Create footer.php

---
+++ app/views/template/footer.php
@@ -0,0 +1,10 @@
+        <!-- Footer -->
+        <footer>
+            <p>Alamat | Kontak | Laporan | Pengaturan</p>
+        </footer>
+    </main>
+
+
+    <script src="<?= BASE_URL; ?>js/script.js"></script>
+</body>
+</html>
